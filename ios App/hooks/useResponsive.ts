2c9a515ca7d49fb9a1a01feb35492c8ea404a1b3
Muhammad Harris <harris20014@gmail.com>
2026-08-16 14:46:54 +0500

    Split to iOS & Android Folder

--- ios App/hooks/useResponsive.ts
+++
@@ -1,9 +0,0 @@
-import { useWindowDimensions, ScaledSize } from 'react-native';
-import { getResponsiveValues } from '../utils/responsive';
-
-export function useResponsive() {
-  const dimensions = useWindowDimensions();
-  return getResponsiveValues(dimensions);
-}
-
-export type ResponsiveValues = ReturnType<typeof getResponsiveValues>;
